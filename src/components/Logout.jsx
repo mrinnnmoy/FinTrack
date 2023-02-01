eba4e42d3f88dc085e738ce42bf87f687e1ae7a6
mrinnnmoy <mrinmoy202000@gmail.com>
2023-02-01 10:20:45 +0530

    connect logout-to-backend

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
-// import { useHistory } from 'react-router';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router';
 
 const Logout = () => {
+
+    const history = useHistory();
+    const logout = async () => {
+        try {
+            const res = await fetch('/logout', {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            });
+
+            if (res.status === 401 || !res) {
+                window.alert("Please Logout Later");
+            } else {
+                history.push('/');
+                window.location.reload()
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        logout();
+    });
 
     return (
         <div>
